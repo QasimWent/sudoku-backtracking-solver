264c38968f8d9e9a4aa80531b9d8276a02660675
QasimWent <wentzelqasim2@gmail.com>
2019-12-29 15:36:22 +0200

    Add validate board functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <string>
 
+struct IndexPair
+{
+    int row_index;
+    int col_index;
+
+    bool operator!=(const IndexPair& rhs) const
+    {
+        if (this->row_index != rhs.row_index && this->col_index != rhs.col_index)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
+
 void print_board(const int (&board)[9][9])
 {
     int rows = sizeof(board) / sizeof(board[0]);
@@ -25,9 +44,67 @@ void print_board(const int (&board)[9][9])
             {
                 std::cout << board[i][j] << " ";
             }
-            
         }
     }
+}
+
+IndexPair find_empty(const int (&board)[9][9])
+{
+    int rows = sizeof(board) / sizeof(board[0]);
+    int cols = sizeof(board[0]) / sizeof(int);
+    for (size_t i = 0; i != rows; ++i)
+    {
+        for (size_t j = 0; j != cols; ++j)
+        {
+            if (board[i][j] == 0)
+            {
+                return {i, j};
+            }
+        }
+    }
+}
+
+bool is_valid(const int (&board)[9][9], int number, IndexPair position)
+{
+    int rows = sizeof(board) / sizeof(board[0]);
+    int cols = sizeof(board[0]) / sizeof(int);
+
+    // Check the row - loop through each column in given row
+    for (size_t i = 0; i != cols; ++i)
+    {
+        if (board[position.row_index][i] == number && position.col_index != i)
+        {
+            return false;
+        }
+    }
+
+    // Check the column - loop through each row in given column
+    for (size_t i = 0; i != rows; ++i)
+    {
+        if (board[i][position.col_index] == number && position.row_index != i)
+        {
+            return false;
+        }
+    }
+
+    // Determine 3x3 cube
+    int box_x = position.col_index / 3;
+    int box_y = position.row_index / 3;
+
+    // Check 3x3 cube - loop through each element in cube
+    IndexPair pair;
+    for (size_t i = box_y * 3; i != box_y * 3 + 3; ++i)
+    {
+        for (size_t j = box_x * 3; j != box_x * 3 + 3; ++j)
+        {
+            if (board[i][j] == number && position != pair)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 int main()
